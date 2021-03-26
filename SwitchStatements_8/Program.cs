3bc3a422be59ea98c1c19057102c1288c37f9094
IVProb <jnoojinjr@gmail.com>
2021-03-26 10:16:25 -0500

    switch statment added

---
+++ SwitchStatements_8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SwitchStatements_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject?");
+            var subject = Console.ReadLine().ToLower();
+
+
+            switch (subject)
+            {
+                case "biology":
+                    Console.WriteLine("Your Favorite course is {0}! Mine too!", subject);
+                    break;
+                case "math":
+                    Console.WriteLine("Your Favorite course is {0}! Mine too!", subject);
+                    break;
+                case "english":
+                    Console.WriteLine("Your Favorite course is {0}! Mine too!", subject);
+                    break;
+                case "art":
+                    Console.WriteLine("Your Favorite course is {0}! Mine too!", subject);
+                    break;
+                case "music":
+                    Console.WriteLine("Your Favorite course is {0}! Mine too!", subject);
+                    break;
+                default:
+                    Console.WriteLine("Oh wow! I haven't taken that subject before. {0} sounds fun.", subject);
+                    break;
+            }
+        }
+    }
+}
